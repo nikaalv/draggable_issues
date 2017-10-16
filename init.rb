042b548f4df18d25b5f024b2c2141155f9fbc22e
Edgar Meinart <edgars@meinarts.name>
2017-10-16 19:55:24 +0300

    Version bump

--- init.rb
+++ init.rb
@@ -6,7 +6,7 @@ Redmine::Plugin.register :draggable_issues do
   name 'Draggable Issues plugin for Redmine'
   author 'Veronkia Meinarte'
   description 'This is a simple plugin for making droppable issues table rows'
-  version '0.0.1'
+  version '0.0.2'
   url 'https://github.com/nikaalv/draggable_issues'
   author_url 'https://github.com/nikaalv'
 end
